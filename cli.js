1060fae4d9fc52c1d394b8da1d39255bf78f520c
doowb <brian.woodward@gmail.com>
2017-11-01 18:44:17 -0400

    better output

--- cli.js
+++ cli.js
@@ -1,31 +1,46 @@
 #!/usr/bin/env node
 'use strict';
 
-const args = require('minimist')(process.argv.slice(2));
+const bold = require('ansi-bold');
+const cyan = require('ansi-cyan');
+const gray = require('ansi-gray');
+const wrap = require('word-wrap');
+const longest = require('longest');
+const minimist = require('minimist');
+const repeat = require('repeat-string');
+
 const synonyms = require('./');
 
-const results = {};
-for (let i = 0; i < args._.length; i++) {
-  let word = args._[i];
-  results[word] = synonyms(word);
-}
+const args = minimist(process.argv.slice(2));
 
-for (let i = 0; i < args._.length; i++) {
-  let word = args._[i];
-  console.log(`${word}:`);
-  if (typeof results[word].nouns === 'undefined' && typeof results[word].verbs === 'undefined') {
-    console.log(`  No results found.`);
+const words = args._;
+const wordLen = longest(words).length + 3;
+const results = synonyms(words);
+
+for (let i = 0; i < words.length; i++) {
+  let word = words[i];
+  let nouns = results[word].nouns;
+  let verbs = results[word].verbs;
+  let output = cyan(word) + repeat(' ', wordLen - word.length);
+  let indent = wordLen + 7;
+
+  if (!nouns.length && !verbs.length) {
+    output += gray('No synonyms found.') + '\n';
   }
 
-  if (typeof results[word].nouns !== 'undefined') {
-    console.log(`  Nouns: ${results[word].nouns.join(', ')}`);
-    console.log();
+  if (nouns.length) {
+    output += bold('Nouns: ');
+    nouns = wrap(nouns.join(', '), {indent: repeat(' ', indent)});
+    output += gray(nouns.substr(indent)) + '\n';
   }
 
-  if (typeof results[word].verbs !== 'undefined') {
-    console.log(`  Verbs: ${results[word].verbs.join(', ')}`);
-    console.log();
+  if (verbs.length) {
+    if (output.charAt(output.length - 1) === '\n') {
+      output += repeat(' ', wordLen);
+    }
+    output += bold('Verbs: ');
+    verbs = wrap(verbs.join(', '), {indent: repeat(' ', indent)});
+    output += gray(verbs.substr(indent)) + '\n';
   }
-
-  console.log();
+  console.log(output);
 }
--- index.js
+++ index.js
@@ -2,10 +2,22 @@
 
 var synonyms = require('synonyms');
 
-module.exports = function(word) {
-  var results = synonyms(word) || {};
-  return {
-    nouns: results.n,
-    verbs: results.v
-  };
+module.exports = function(words) {
+  const results = {};
+
+  words = arrayify(words);
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    let found = synonyms(word);
+    results[word] = {
+      nouns: arrayify(found.n),
+      verbs: arrayify(found.v)
+    };
+  }
+
+  return results;
 };
+
+function arrayify(val) {
+  return val ? (Array.isArray(val) ? val : [val]) : [];
+}
